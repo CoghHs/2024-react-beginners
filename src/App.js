e023f366ccbd47af426078a3cc3bbfcc658010b9
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2024-06-22 16:47:09 +0900

    fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <h1>hello</h1>\
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  const getCharacter = async () => {
+    const json = await (
+      await fetch(
+        `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+      )
+    ).json();
+    setCharacters(json.data.results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getCharacter();
+  }, []);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
